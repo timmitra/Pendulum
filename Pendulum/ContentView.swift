28ec1a5af6c5c7b46065b7ea099688dee0018a9f
Tim Mitra <tim@it-guy.com>
2024-10-02 11:24:17 -0400

    added enity relationships

--- Pendulum/ContentView.swift
+++ Pendulum/ContentView.swift
@@ -19,28 +19,20 @@ struct ContentView: View {
 
     var body: some View {
         RealityView { content in
-//                    let model = ModelEntity(
-//                                 mesh: .generateSphere(radius: 0.05),
-//                                 materials: [SimpleMaterial(color: .white, isMetallic: true)])
-//                    content.add(model)
-                //}
-            
-            let ballEntity = ModelEntity(
-                mesh: .generateSphere(radius: 0.05),
-                materials: [SimpleMaterial(color: .white, isMetallic: true)])
-//            ballEntity.position.x = 0.0
-            ballEntity.position.y = -0.425
-//            ballEntity.position.y = -1.5
-            ballEntity.components.set(CollisionComponent(shapes: [.generateSphere(radius: 0.05)]))
-            content.add(ballEntity)
             
             let attachmentEntity = ModelEntity(mesh: .generateBox(width: 0.2, height: 0.025, depth: 0.2), materials: [SimpleMaterial(color: .green, isMetallic: false)])
             attachmentEntity.position.y = 0.45
             content.add(attachmentEntity)
             
-                let stringEntity = ModelEntity(mesh: .generateCylinder(height: 1.5, radius: 0.005), materials: [SimpleMaterial(color: .gray, isMetallic: false)])
-            //    stringEntity.position = [0.0, 1.0, -1.5]
-                content.add(stringEntity)
+            let stringEntity = ModelEntity(mesh: .generateCylinder(height: 1.5, radius: 0.005), materials: [SimpleMaterial(color: .gray, isMetallic: false)])
+            attachmentEntity.addChild(stringEntity)
+            
+            let ballEntity = ModelEntity(
+                mesh: .generateSphere(radius: 0.05),
+                materials: [SimpleMaterial(color: .white, isMetallic: true)])
+            ballEntity.position.y = -0.75
+            ballEntity.components.set(CollisionComponent(shapes: [.generateSphere(radius: 0.05)]))
+            stringEntity.addChild(ballEntity)
            }
             
         }
