5bc38bd962c81982cc67120ec801178a826fc910
Tim Mitra <tim@it-guy.com>
2024-10-02 01:56:05 -0400

    Initial Commit

---
+++ Pendulum/ImmersiveView.swift
@@ -0,0 +1,29 @@
+//
+// ---------------------------- //
+// Original Project: Pendulum
+// Created on 2024-10-02 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(scene)
+            }
+        }
+    }
+}
+
+#Preview(immersionStyle: .mixed) {
+    ImmersiveView()
+}
---
+++ Pendulum/PendulumApp.swift
@@ -0,0 +1,25 @@
+//
+// ---------------------------- //
+// Original Project: Pendulum
+// Created on 2024-10-02 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct PendulumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
